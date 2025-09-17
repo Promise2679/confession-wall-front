4c98d9eaa3b9dbe04cedd18ac4c1496a37c51f1d
Luminith <61924789+Promise2679@users.noreply.github.com>
2025-09-17 10:29:40 +0800

    fix: 用flex修复了注销按钮小屏时飞出屏幕的问题

--- src/views/components/aside.vue
+++ src/views/components/aside.vue
@@ -18,15 +18,17 @@ const logout = () => {
             <img class="avatar" src="@/asset/default_avatar.webp">
             <span class="name">{{ store.username.length ? store.username : '未登录' }}</span>
         </div>
-        <hr class="translate">
         <RouterLink to="/" class="translate btn">首页</RouterLink>
         <RouterLink to="/profile" class="translate btn">个人中心</RouterLink>
+        <div style="flex: auto;"></div>
         <div @click="logout" class="logout">注销</div>
     </div>
 </el-affix></template>
 
 <style scoped>
 .aside {
+    display: flex;
+    flex-direction: column;
     min-height: 100vh;
     height: 100%;
     width: 300px;
@@ -38,7 +40,6 @@ const logout = () => {
     flex-direction: row;
     gap: 10px;
     align-items: center;
-    position: relative;
     transform: translate(20px, 20px);
 }
 
@@ -69,10 +70,9 @@ const logout = () => {
 
 .logout {
     display: flex;
-    position: relative;
-    bottom: -650px;
     width: 100%;
     height: 50px;
+    margin-bottom: 10px;
     align-items: center;
     justify-content: center;
     color: red;
